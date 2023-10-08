a271b7cb817cbbe5d8fde0924e21c77bdf2fee8e
Yerlan Koishybaiuly <yk.kaen@gmail.com>
2023-10-08 17:58:15 +0600

    New change

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print("Hello World")
+name = input("Введите наименование товара: ")
+price = int(input("Введите цену товара (рубли): "))
+weight = int(input("Введите вес товара (кг): "))
+quantity = int(input("Введите количество: "))
+total = price * weight
+
+print("Чек")
+print(f"{name} - {weight} кг - {price} руб/кг")
+print(f"Итого: {total} руб")
+print(f"Внесено: {quantity} руб")
+print(f"Сдача: {quantity - total} руб")
+
 
