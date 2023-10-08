18b4401196e29621524423d6d0ef89f6faf062c2
Yerlan Koishybaiuly <yk.kaen@gmail.com>
2023-10-08 18:34:43 +0600

    Add new ts file

---
+++ type.ts
@@ -0,0 +1,16 @@
+// Define a class called "Person"
+class Person {
+  // Constructor to initialize the properties
+  constructor(public name: string, public age: number) {}
+
+  // Method to display information about the person
+  displayInfo() {
+    console.log(`Name: ${this.name}, Age: ${this.age} years old`);
+  }
+}
+
+// Create an instance of the Person class
+const person1 = new Person("Alice", 30);
+
+// Call the displayInfo method to display the information
+person1.displayInfo();
